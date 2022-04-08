62585b1063cc7ba3e0b8337a6eeea719e7ea00b7
Paul Kirby <paulrdkirby@gmail.com>
2022-04-08 15:45:48 -0500

    init commit

---
+++ server.ts
@@ -0,0 +1,19 @@
+// Require the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3001)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
