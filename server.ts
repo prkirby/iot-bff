f44ec5178c09fb33b5e8efdf874ad057756f96dc
Paul Kirby <paulrdkirby@gmail.com>
2022-04-21 02:20:07 -0500

    change server

--- server.ts
+++ server.ts
@@ -1,35 +1,37 @@
 // Require the framework and instantiate it
 import 'dotenv/config'
 import Fastify from 'fastify'
-import fastifyCors from 'fastify-cors'
+// import fastifyCors from 'fastify-cors'
 import mqtt from 'mqtt'
 
 // Would eventually like to packagize this
 import { lightsTopics } from '../iot-broker/schema'
 
-const fastify = Fastify({ logger: true })
+const fastify = Fastify({
+  logger: true,
+})
 
 // Pass in allowable host names
 // const CORSHosts = process.env.CORS_URLS?.split(',')
 
-fastify.register(fastifyCors, {
-  origin: (_origin, cb) => {
-    cb(null, true)
-    // if (origin === undefined) {
-    //   cb(null, true)
-    //   return
-    // }
+// fastify.register(fastifyCors, {
+//   origin: (_origin, cb) => {
+//     cb(null, true)
+// if (origin === undefined) {
+//   cb(null, true)
+//   return
+// }
 
-    // const hostname = new URL(origin).hostname
-    // if (CORSHosts?.includes(hostname)) {
-    //   //  Request from localhost will pass
-    //   cb(null, true)
-    //   return
-    // }
-    // // Generate an error on other origins, disabling access
-    // cb(new Error('Not allowed'), false)
-  },
-})
+// const hostname = new URL(origin).hostname
+// if (CORSHosts?.includes(hostname)) {
+//   //  Request from localhost will pass
+//   cb(null, true)
+//   return
+// }
+// // Generate an error on other origins, disabling access
+// cb(new Error('Not allowed'), false)
+//   },
+// })
 
 const host = process.env.HOST || 'mqtt://localhost:1883'
 
@@ -67,7 +69,7 @@ const port = parseInt(process.env.PORT || '3001')
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(port)
+    await fastify.listen({ port, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
